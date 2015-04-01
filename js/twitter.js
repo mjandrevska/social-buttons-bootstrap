bfa82be4b5a22e44bbdad5657f444d7f06ad4a5a
Monika <monikjandrevska@hotmail.com>
2015-04-01 20:39:49 +0200

    Twitter social button made

---
+++ js/twitter.js
@@ -0,0 +1,16 @@
+function getTweets(element){
+	var $elem = $(element);
+	var purl = $elem.attr('data-url');
+	var url = 'http://cdn.syndication.twitter.com/widgets/tweetbutton/count.json';
+	$.ajax(url, {
+	  	type: 'GET',
+	    dataType: 'jsonp',
+	    url: url,
+	    data: {
+	    	url: purl,
+	      	callback: '?'
+		}
+    }).then(function(response){
+    	$elem.text(response.count);
+    });
+}
