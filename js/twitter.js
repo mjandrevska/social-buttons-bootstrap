665a7b1231951247d7a88471cbff829b491d1dac
Monika Jandrevska <monikajandrevska@gmail.com>
2015-04-01 20:42:27 +0200

    Tabulation fixed

--- js/twitter.js
+++ js/twitter.js
@@ -3,14 +3,14 @@ function getTweets(element){
 	var purl = $elem.attr('data-url');
 	var url = 'http://cdn.syndication.twitter.com/widgets/tweetbutton/count.json';
 	$.ajax(url, {
-	  	type: 'GET',
-	    dataType: 'jsonp',
-	    url: url,
-	    data: {
-	    	url: purl,
-	      	callback: '?'
+		type: 'GET',
+		dataType: 'jsonp',
+		url: url,
+		data: {
+			url: purl,
+			callback: '?'
 		}
-    }).then(function(response){
-    	$elem.text(response.count);
-    });
+	}).then(function(response){
+		$elem.text(response.count);
+	});
 }
